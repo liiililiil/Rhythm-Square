63a8542a3d027ae9d51fc7f219d63581aa0428f5
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-23 19:22:31 +0900

    Addressable을 위해 코드를 변경하였습니다.

--- Assets/Scripts/ScriptableObjects/AddressableManagement.cs
+++ Assets/Scripts/ScriptableObjects/AddressableManagement.cs
@@ -1,6 +1,6 @@
-using AudioManagement;
 using UnityEngine;
-
+using AudioManagement;
+using UnityEngine.AddressableAssets;
 
 /// <summary>
 /// 자산 관리를 위한 스크립터블 오브젝트들을 정의합니다.
@@ -10,11 +10,10 @@ namespace AddressableManagement
     /// <summary>
     /// 노래에 맞는 오브젝트을 로드하기 위한 스크립터블 오브젝트입니다.
     /// </summary>
-    [CreateAssetMenu(fileName = "Playerable", menuName = "AddressableManagement/New Playerable")]
+    [CreateAssetMenu(fileName = "Playerable", menuName = "AddressableManagement/New Playerable Music Asset")]
     public class PlayerableMusicAsset : ScriptableObject
     {
-        public PlayableMusic playableMusic;
-        public GameObject addressableGameObject;
-
+        public AssetReferenceT<PlayableMusic> playableMusic;
+        public AssetReferenceGameObject addressableGameObject;
     }
 }
