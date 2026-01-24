546a75e823aa0177ed87bac7b268a5230f23e61e
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-24 22:23:53 +0900

    기본 상태 추가 및 애니메이션을 위한 로직 추가

--- Assets/Scripts/MainMenu/StateChanger/RectPositionChanger.cs
+++ Assets/Scripts/MainMenu/StateChanger/RectPositionChanger.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 using Types;
 using MainMenu.StateChanger;
+using System.Collections;
+using Easing;
+using Utils;
 
 
 public class RectPositionChanger : StateChanger
 {
     [SerializeField]
     private MenuStateChange<Vector2>[] positionState;
-    
+    [SerializeField]
+    private MenuStateDefault<Vector2> defaultPosition;
+
     private RectTransform rectTransform;
+
+    private Coroutine coroutine;
 
     private void Awake() {
         rectTransform = GetComponent<RectTransform>();
@@ -20,13 +27,34 @@ public class RectPositionChanger : StateChanger
         {
             if(stateChange.TargetState == newState)
             {
-                rectTransform.anchoredPosition = stateChange.Value;
+                this.SafeStartCoroutine(ref coroutine, ChangePositionCoroutine(stateChange.Value, stateChange.Duration, stateChange.EaseType));
                 return;
             }
         }
+
+        //기본값으로 변경
+        this.SafeStartCoroutine(ref coroutine, ChangePositionCoroutine(defaultPosition.Value, defaultPosition.Duration, defaultPosition.EaseType));
+    }
+
+    IEnumerator ChangePositionCoroutine(Vector2 targetPos, float duration, EaseType easeType)
+    {
+        Vector2 startPos = rectTransform.anchoredPosition;
+        float elapsed = 0f;
+
+        while(elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+            t = Ease.Easing(easeType, t);
+
+            rectTransform.anchoredPosition = Vector2.LerpUnclamped(startPos, targetPos, t);
+
+            yield return null;
+        }
+
+        rectTransform.anchoredPosition = targetPos;
+
+        this.SafeStopCoroutine(ref coroutine);
     }
 
 }
-
-
-// 메뉴 상태에 따른 변화
