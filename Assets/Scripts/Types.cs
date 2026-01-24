2b3f9f7143919a803774ceb506e847d4496d45af
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-24 22:21:51 +0900

    메뉴 상태 추가

--- Assets/Scripts/Types.cs
+++ Assets/Scripts/Types.cs
@@ -1,29 +1,38 @@
+using System;
+
 namespace Types
 {
     // 메뉴 상태들
-    public enum MenuState
+    public enum MenuState : byte
     {
         //초기 설정 로딩
-        InitLoading,
+        InitLoading = 0,
+        MainWaitng = 1,
 
         //타이틀
-        Main,
+        Main = 2,
 
         //노래 선택화면
-        MusicSelection,
+        MusicSelection = 3,
+
+        //스테이지 로딩
+        MusicLoading = 4,
 
         //설정 화면
-        Setting,
+        Setting = 5,
 
         //프로그램 종료 경고
-        ExitWarning,
+        ExitWarning = 6,
+
+        //프로그램 종료 대기
+        ExitWating = 7,
 
         //제작자
-        Credits
+        Credits = 8
     }
 
     // 설정
-    public struct Settings
+    public class Settings
     {
         public float musicVolume;
         public float sfxVolume;
@@ -31,10 +40,12 @@ namespace Types
 
 
     // 실수 범위
+    [Serializable]
     public struct FloatRange
     {
         public float start;
         public float end;
+        
     }
 
 
