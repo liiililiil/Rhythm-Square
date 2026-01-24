4a119abd5cdc4589bae4a1af1df8c307705226f0
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-24 22:21:41 +0900

    설정 매니저 추가

---
+++ Assets/Scripts/SettingManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Types;
+public class SettingManager : Managers<SettingManager>
+{
+    public Settings setting {get; set;} =  new Settings(); 
+    private void Awake() {
+        Singleton();
+    }
+    private void Start() {
+        setting.musicVolume = 1;
+    }
+}
