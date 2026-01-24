a55b3ec692edeb408bac01f826c1bb1441ccbbea
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-01-24 17:12:30 +0900

    메뉴 상태 변경 매니저 구현

--- Assets/Scripts/MainMenu/MenuStateManager.cs
+++ Assets/Scripts/MainMenu/MenuStateManager.cs
@@ -1,12 +1,27 @@
-using UnityEngine;
+using SimpleActions;
+using Types;
 
 
 
-public class MenuStateManager : MonoBehaviour
+public class MenuStateManager : Managers<MenuStateManager>
 {
+    public SimpleEvent<MenuState> onMenuStateChanged = new SimpleEvent<MenuState>();
+
+    private void Awake() {
+        Singleton(false);
+    }
     
+    //메뉴 변경(버튼 용)
     public void ChangeMenuState(int newState)
     {
         //메뉴 상태 변경 처리
+        onMenuStateChanged.Invoke((MenuState)newState);
     }
+
+    //메뉴 변경
+    public void ChangeMenuState(MenuState newState)
+    {
+        //메뉴 상태 변경 처리
+        onMenuStateChanged.Invoke(newState);
+    }   
 }
