99e2f78e156aa64a55eb116c15bad2a5b2c8179f
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-02-06 05:00:30 +0900

    설정 캡슐화 및 설정이 바뀌면 invoke되는 이벤트 추가

--- Assets/Scripts/SettingManager.cs
+++ Assets/Scripts/SettingManager.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using Types;
+using SimpleActions;
 public class SettingManager : Managers<SettingManager>
 {
-    public Settings setting {get; set;} =  new Settings(); 
+    private Setting _setting = new Setting();
+    public Setting setting {get => _setting; private set => _setting = value;}
+
+    public SimpleEvent<Setting> onChangeSetting = new SimpleEvent<Setting>();
+
     private void Awake() {
         Singleton();
     }
-    private void Start() {
-        setting.musicVolume = 0.6f;
+    private void Start()
+    {
+        onChangeSetting.Invoke(setting);
     }
 }
