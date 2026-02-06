15cb0006cc3e7c99bcf00898f9eb980e914cc210
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-02-07 05:09:00 +0900

    한번만 invoke되고 초기화되는 이벤트 추가

--- Assets/SimpleActions/Script/SimpleActions.cs
+++ Assets/SimpleActions/Script/SimpleActions.cs
@@ -1,8 +1,42 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace SimpleActions
 {
+    //한번만 작동하고 자동 해제
+    public class OneTimeEvent
+    {
+        List<Action> actions = new List<Action>();
+
+        public void AddListener(Action action)
+        {
+            actions.Add(action);
+        }
+
+        public void RemoveListener(Action action)
+        {
+            actions.Remove(action);
+        }
+
+        public void Invoke()
+        {
+            //임시 액션에 리스트를 복사하고 원본 리스트는 초기화
+            List<Action> tempActions = new List<Action>(actions);
+            actions.Clear();
+
+            for(int i = tempActions.Count -1; i >=0; i--) {
+                try
+                {
+                    tempActions[i].Invoke();
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError(ex);
+                }
+            }
+        }
+    } 
     //경량화된 이벤트
     public class SimpleEvent
     {
@@ -22,14 +56,7 @@ namespace SimpleActions
         {
             for(int i = actions.Count -1; i >=0; i--)
             {
-                try
-                {
-                    actions[i].Invoke();
-                }
-                catch(NullReferenceException)
-                {
-                    actions.RemoveAt(i);
-                }
+                actions[i].Invoke();
             }
         }
     }
@@ -75,14 +102,7 @@ namespace SimpleActions
         {
             for (int i = actions.Count - 1; i >= 0; i--)
             {
-                try
-                {
-                    actions[i].Invoke(param1, param2);
-                }
-                catch (NullReferenceException)
-                {
-                    actions.RemoveAt(i);
-                }
+                actions[i].Invoke(param1, param2);
             }
         }
     }
@@ -105,14 +125,7 @@ namespace SimpleActions
         {
             for (int i = actions.Count - 1; i >= 0; i--)
             {
-                try
-                {
-                    actions[i].Invoke(param1, param2, param3);
-                }
-                catch (NullReferenceException)
-                {
-                    actions.RemoveAt(i);
-                }
+                actions[i].Invoke(param1, param2, param3);
             }
         }
     }
