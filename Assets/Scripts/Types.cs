3928a4afe757055774705aeb47d36a6573f7ffa3
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-02-07 05:05:16 +0900

    타입 네임스페이스 세분화 적용 및 다른 곳에 있던 타입들을 여기로 전부 이동

--- Assets/Scripts/Types.cs
+++ Assets/Scripts/Types.cs
@@ -1,6 +1,19 @@
 using System;
-
+using System.Collections.Generic;
+using SimpleActions;
+using SimpleEasing;
 namespace Types
+{
+    // 실수 범위
+    [Serializable]
+    public struct FloatRange
+    {
+        public float start;
+        public float end;
+        
+    }
+}
+namespace Types.Menu
 {
     // 메뉴 상태들
     public enum MenuState : byte
@@ -34,8 +47,9 @@ namespace Types
     // 언어들
     public enum Language : byte
     {
-        Korean = 0,
-        English = 1,   
+        Empty = 0,
+        Korean = 1,
+        English = 2,
     }
 
 
@@ -63,18 +77,125 @@ namespace Types
         }
     }
 
+}
 
-
-    // 실수 범위
+namespace Types.Menu.StateChange
+{
     [Serializable]
-    public struct FloatRange
+    public abstract class StateChange<T>
     {
-        public float start;
-        public float end;
-        
+        public T value;
     }
 
+    [Serializable]
+    public abstract class MenuStateChange<T> : StateChange<T>
+    {
+        public MenuState targetState;
+    }
 
+    public abstract class MenuStateDefault<T> : StateChange<T>
+    {
+    }
 
+    // 지연후 실행되는 목록
+    [Serializable]
+    public class DelayedMenuStateChange<T> : MenuStateChange<T>
+    {
+        public float delay;
+    }
 
+    // 기본
+    [Serializable]
+    public class DelayedMenuStateDefault<T> : MenuStateDefault<T>
+    {
+        public float delay;
+    }
+
+    // 느리게 실행되는 목록
+    [Serializable]
+    public class SlowMenuStateChange<T> : MenuStateChange<T>
+    {
+        public float duration;
+        public EaseType easeType;
+    }
+    
+    // 기본
+    [Serializable]
+    public class SlowMenuStateDefault<T> : MenuStateDefault<T>
+    {
+        public float duration;
+        public EaseType easeType;
+    }
+
+}
+
+namespace Type.Addressable
+{
+    // 에셋 로딩을 통합 관리하기위한 클래스
+    public class AddressableLoadingRecoder
+    {
+        private int index;
+        private int leftPrograss;
+
+        private List<float> prograssList = new List<float>();
+        private bool isRecode = false;
+
+        private SimpleEvent<float> OnStartLoading = new SimpleEvent<float>();
+        public void OpenRecode()
+        {
+            index = 0;
+            leftPrograss = 0;
+            prograssList.Clear();
+
+            isRecode = true;
+        }
+
+        public void CloseRecode()
+        {
+            isRecode = false;
+        }
+        
+        public void StartLoading(out int startIndex)
+        {
+            if(!isRecode)
+            {
+                UnityEngine.Debug.LogError("기록 중이지 않습니다!");
+                startIndex = -1;
+                return;
+            }
+
+            startIndex = index;
+            index++;
+            leftPrograss++;
+
+            prograssList.Add(0);
+        }
+
+        public void CompleteLoading(int loadIndex)
+        {
+            leftPrograss--;
+            prograssList[loadIndex] = 1;
+        }
+
+        public float GetTotalPrograss()
+        {
+            float total = 0;
+            foreach(var p in prograssList)
+            {
+                total += p;
+            }
+
+            return total / prograssList.Count;
+        }
+
+        public float GetLoadingPrograss(int loadIndex)
+        {
+            return prograssList[loadIndex];
+        }
+
+        public bool IsAllComplete()
+        {
+            return leftPrograss <= 0;
+        }
+    }
 }
