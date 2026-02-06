e697b0a4745efec2c6b4127debd511942a1b72a6
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-02-07 05:07:12 +0900

    언어 변경 버튼 추가 및 구현

---
+++ Assets/Scripts/MainMenu/OnClick/Language Switch.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+using SimpleEasing;
+using Types.Menu;
+using System.Collections;
+using Utils;
+
+public class LanguageSwitch : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField]
+    private GameObject korean;
+    [SerializeField]
+    private GameObject english;
+
+    [Space(10), SerializeField]
+    private GameObject bar;
+
+    private Coroutine BarCoroutine;
+    private Coroutine koreanCoroutine;
+    private Coroutine englishCoroutine;
+
+    private const float BAR_POSITION_X = 200f;
+    private const float BAR_POSITION_Y = -150f;
+    private const float DURATION = 0.5f;
+    private const EaseType EASETYPE = EaseType.OutCubic;
+    private const float TEXT_POSITION_Y = 25f;
+
+    private void Start() {
+        SettingManager.Instance.onChangeSetting.AddListener(OnChangeSetting);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ChangePosition(Language.Empty);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ChangePosition(SettingManager.Instance.setting.language);
+    }
+
+    public void OnPointerClick(PointerEventData exentData)
+    {
+        switch (SettingManager.Instance.setting.language)
+        {
+            case Language.Korean:
+                SettingManager.Instance.ChangeLanguage(Language.English);
+                break;
+            case Language.English:
+                SettingManager.Instance.ChangeLanguage(Language.Korean);
+                break;
+        }
+    }
+
+    private void OnChangeSetting(Setting setting)
+    {
+        ChangePosition(setting.language);
+    }
+
+    private void ChangePosition(Language language)
+    {
+        int value = 0;
+        switch (language)
+        {
+            case Language.Korean:
+                value = 1;
+                break;
+
+            case Language.English:
+                value = -1;
+                break;
+
+            default:
+                value = 0;
+                break;
+        }
+
+
+        //적용
+        this.SafeStartCoroutine(ref BarCoroutine, SlowChangePosition(bar, (bar.transform as RectTransform).anchoredPosition, new Vector2(value *BAR_POSITION_X, BAR_POSITION_Y), DURATION, EASETYPE));
+        this.SafeStartCoroutine(ref koreanCoroutine, SlowChangePosition(korean, (korean.transform as RectTransform).anchoredPosition, new Vector2(-200, -value *TEXT_POSITION_Y - 25),DURATION, EASETYPE));
+        this.SafeStartCoroutine(ref englishCoroutine, SlowChangePosition(english, (english.transform as RectTransform).anchoredPosition, new Vector2(200, value *TEXT_POSITION_Y - 25),DURATION, EASETYPE));
+    }
+
+    private IEnumerator SlowChangePosition(GameObject gameObject, Vector2 start, Vector2 end, float duration, EaseType easeType)
+    {
+        RectTransform rt = gameObject.GetComponent<RectTransform>();
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / duration);
+
+            t = Ease.Easing(t, easeType);
+
+            Vector2 newPosition = Vector2.Lerp(start, end, t);
+            rt.anchoredPosition = newPosition;
+            yield return null;
+        }
+
+        // 보정
+        rt.anchoredPosition = end;
+
+    }
+
+
+}
