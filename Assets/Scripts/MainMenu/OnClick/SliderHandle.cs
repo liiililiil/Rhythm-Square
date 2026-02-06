0e2ae59360aed8b9966b87db253edf9d8fcf6a74
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-02-07 05:07:39 +0900

    핸들에 애니메이션을 위한 클래스 생성

---
+++ Assets/Scripts/MainMenu/OnClick/SliderHandle.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SliderHandle : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
