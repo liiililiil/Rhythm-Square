a09e5a8db33cd9677e408a8757511a5bcb3f511f
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-02-17 10:51:28 +0900

    언어 설정 구현 및 설정 변경을 이벤트의 매계변수로 사용하지 않고 직접 설정 메니저를 참조해야되도록 변경

--- Assets/Scripts/SettingManager.cs
+++ Assets/Scripts/SettingManager.cs
@@ -1,24 +1,42 @@
-using UnityEngine;
 using Types.Menu;
 using SimpleActions;
+using UnityEngine;
 public class SettingManager : Managers<SettingManager>
 {
     private Setting _setting = new Setting();
     public Setting setting {get => _setting; private set => _setting = value;}
 
-    public SimpleEvent<Setting> onChangeSetting = new SimpleEvent<Setting>();
+    public SimpleEvent onChangeSetting = new SimpleEvent();
 
     private void Awake() {
         Singleton();
     }
     private void Start()
     {
-        onChangeSetting.Invoke(setting);
+        onChangeSetting.Invoke();
     }
 
-    public void ChangeLanguage(Language language)
+    public void SetLanguage(Language language, bool isSilence = false)
     {
         setting.language = language;
-        onChangeSetting.Invoke(setting);
+        if(!isSilence) onChangeSetting.Invoke();
     }
+
+    public void SetVolume(float value, Types.Menu.AudioType type, bool isSilence = false)
+    {
+        setting.volumes.SettMatchedAudio(type, value);
+
+        if(!isSilence) onChangeSetting.Invoke();
+    }
+
+    public void SetOffset(int value, bool isSilence = false)
+    {
+        value = Mathf.Clamp(value,-500,500);
+        setting.offset = value;
+
+        if(!isSilence) onChangeSetting.Invoke();
+    }
+
+    
+
 }
