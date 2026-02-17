2f8a304a9c015c74edd7c3f6185a000173b918f6
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-02-17 19:40:41 +0900

    어드레서블 스프라이트 스크립터블 오브젝트 정의

---
+++ Assets/Scripts/ScriptableObjects/AddressableManagement/AddressableSprite.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+using AudioManagement;
+using Types.Addressable.Table;
+namespace AddressableManagement
+{
+    /// <summary>
+    /// 스프라이트을 나타내는 스크립터블 오브젝트입니다.
+    /// </summary>
+    [CreateAssetMenu(fileName = "EmptySpriteAsset", menuName = "SpriteAssets/New AddressableSprite")]
+    public class AddressableSprite : IndexedScriptableObject<SpriteIndex>
+    {
+        public Sprite sprite;
+    }
+}
