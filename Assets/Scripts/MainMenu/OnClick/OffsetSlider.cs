99ed01a4c4aec5041aa6782f181c51947890a836
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-02-17 10:57:39 +0900

    오프셋 설정을 위한 슬라이더 구현

---
+++ Assets/Scripts/MainMenu/OnClick/OffsetSlider.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OffsetSlider : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+    [SerializeField]
+    private InputField inputField;
+
+    private void Awake() {
+        inputField.onSubmit.AddListener(ValueChanged);
+    }
+
+    private void OnEnable() {
+        slider.onValueChanged.AddListener(OnValueChange);
+        SettingManager.Instance.onChangeSetting.AddListener(OnChangeValue);
+    }
+
+    private void OnDisable()
+    {
+        slider.onValueChanged.RemoveListener(OnValueChange);
+        SettingManager.Instance.onChangeSetting.RemoveListener(OnChangeValue);
+    }
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        //인풋 포커싱 중에는 텍스트 변경 중단
+        if(inputField.isFocused) return;
+        inputField.text = SettingManager.Instance.setting.offset.ToString();
+    }
+    
+
+
+    private void ValueChanged(string str)
+    {
+        int result;
+        bool isInt = int.TryParse(str, out result);
+
+        if (!isInt)
+        {
+            Debug.LogError("숫자가 아닌 인풋을 입력받았습니다!");
+            return;
+        }
+
+        SettingManager.Instance.SetOffset(result, false);
+    }
+
+
+    private void OnValueChange(float value)
+    {
+        SettingManager.Instance.SetOffset((int)value, true);
+    }
+    
+    private void OnChangeValue()
+    {
+        slider.value = SettingManager.Instance.setting.offset;
+    }
+}
