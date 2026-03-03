7f48b6e33d3f10b44e402c54734dc1a5caa5b2bc
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-03-04 01:16:19 +0900

    게임 오브젝트를 위한 프리펩 테이블 추가

---
+++ Assets/Scripts/Addressable/Tables/PrefabTable.cs
@@ -0,0 +1,32 @@
+using AddressableManagement;
+using SimpleActions;
+using Types.Addressable;
+using Types.Addressable.Table;
+
+using UnityEngine;
+using Utils;
+namespace Tables.PrefabTable{
+    public class PrefabTable : Managers<PrefabTable>
+    {
+        Loader<AddressablePrefab, PrefabIndex> loader = new Loader<AddressablePrefab, PrefabIndex>();
+        private void Awake() {
+            Singleton(true);
+        }
+
+        public void LoadMainMenu(string tag)
+        {
+            this.SafeStartCoroutine(ref loader.coroutine, loader.LoadingAsset(tag));
+        }
+ 
+        public AddressablePrefab GetPrefab(PrefabIndex prefabIndex)
+        {
+            return loader.table[prefabIndex];
+        }
+
+        //메인메뉴 해제
+        public void Release()
+        {
+            loader.Release();
+        }
+    }
+}
