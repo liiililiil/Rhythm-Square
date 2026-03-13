28b27542b11f983195b3c86bf5f15a4d8396c744
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-03-14 02:41:14 +0900

    에셋을 불러오는 것과 가저오는 것을 분리 및 구현

--- Assets/Scripts/Addressable/AssetLoadManager.cs
+++ Assets/Scripts/Addressable/AssetLoadManager.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
 using SimpleActions;
 using Type.Addressable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
 
 public class AssetLoadManager : Managers<AssetLoadManager>
 {
 
     public SimpleEvent OnMainMenuAssetLoaded = new SimpleEvent();
 
-    public AddressableLoadingRecoder loadingRecoder = new AddressableLoadingRecoder();
+    public LoadingRecoder loadRecoder = new LoadingRecoder();
+    public LoadingRecoder addressableLoadRecoder = new LoadingRecoder();
+
+    private Stack<int> loaderIndex = new Stack<int>();
+    private Stack<Action> loaderAction = new Stack<Action>();
 
     private void Awake()
     {
@@ -19,16 +27,39 @@ public class AssetLoadManager : Managers<AssetLoadManager>
         LoadMainMenu();
     }
 
+    public void LoaderBind(Action action)
+    {
+        int index;
+        loadRecoder.StartLoading(out index);
+
+        loaderIndex.Push(index);
+        loaderAction.Push(action);
+    }
+
+    // 천천히 로딩 하기
+    private IEnumerator ProgressiveLoading()
+    {
+        while(loaderIndex.Count > 0)
+        {
+            loaderAction.Pop().Invoke();
+
+            loadRecoder.CompleteLoading(loaderIndex.Pop());
+
+            yield return null;
+        }
+
+        //완료하면 완료 invoke
+        OnMainMenuAssetLoaded.Invoke();
+    }
     public void LoadMainMenu()
     {
-        loadingRecoder.OpenRecode();
         Tables.PrefabTable.PrefabTable.Instance.LoadMainMenu(Type.Addressable.Tag.Prefab.MAIN_MENU);
         Tables.SpriteTable.SpriteTable.Instance.LoadMainMenu(Type.Addressable.Tag.Sprite.MAIN_MENU);
         Tables.TextTable.TextTable.Instance.Load(Type.Addressable.Tag.Text.MAIN_MENU);
         Tables.MusicTable.MusicTable.Instance.Load();
 
-
-        
-        StartCoroutine(loadingRecoder.WaitForCompleteAllLoading(OnMainMenuAssetLoaded.Invoke));
+        // 어드레서블 로딩이 끝나면 에셋 불러오기를 시작
+        StartCoroutine(addressableLoadRecoder.WaitForCompleteAllLoading(() => StartCoroutine(ProgressiveLoading())));
     }
+    
 }
