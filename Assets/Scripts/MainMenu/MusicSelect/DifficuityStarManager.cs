728d622fccee79d18c5aeb57b15d6463406c2882
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-04-05 20:38:21 +0900

    난이도 별의 설정을 위한 메니저 생성

---
+++ Assets/Scripts/MainMenu/MusicSelect/DifficuityStarManager.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficuityStarManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<DifficuityStar> difficuityStars;
+
+    //(n + m - p) / m
+    // p = 별들의 각 위치
+    // n = 현재 난이도
+    // m = 별들의 수
+
+}
