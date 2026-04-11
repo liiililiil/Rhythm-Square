c9e5ab35082b907059c9fb3603f2d7ff11196e40
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-04-12 04:32:13 +0900

    버튼의 내비게이터 클래스 생성

---
+++ Assets/Scripts/MainMenu/OnClick/ButtonNavigatorManager.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class ButtonNavigatorManager : Managers<ButtonNavigatorManager>
+{
+    public Action OnClickRight;
+    public Action OnClickLeft;
+    public Action OnClickUp;
+    public Action OnClickDown;
+}
