7dd1c6f4ae2d192d9ddecc4a88fc619cd3a8f144
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-04-12 04:31:19 +0900

    Playable 정보와 노래의 info를 가져와서 각각 invoke하는 클래스 구현

--- Assets/Scripts/MainMenu/MusicSelect/PlayableMusicSender.cs
+++ Assets/Scripts/MainMenu/MusicSelect/PlayableMusicSender.cs
@@ -1,16 +1,44 @@
+using AudioManagement;
+using SimpleActions;
+using Tables.MusicTable;
 using UnityEngine;
 
 public class PlayableMusicSender : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    // 데이터가 로딩될때마다 invoke 되는 이벤트
+    public SimpleEvent<PlayableMusic> onLoadPlayerableMusic = new SimpleEvent<PlayableMusic>();
+    public SimpleEvent<MusicInfo> onLoadMusicInfo = new SimpleEvent<MusicInfo>();
+
+    private void Awake()
     {
-        
+    }
+    private void Start()
+    {
+        if (MenuAssetLoadManager.Instance.assetLoadRecoder.IsAllComplete())
+        {
+            OnStartLoad();
+        }
+        else
+        {
+            MenuAssetLoadManager.Instance.AssetLoaderBind(OnStartLoad);
+        }
+    }
+    private void OnStartLoad()
+    {
+        // 우선 playable 음악 정보를 불러와서 각각 invoke 하기
+        PlayableMusic[] playableList = MusicTable.Instance.GetPlayableMusic();
+
+        foreach (PlayableMusic playable in playableList)
+        {
+            onLoadPlayerableMusic.Invoke(playable);
+
+            // 불러온 playable의 index를 기반으로 음악 정보를 불러와서 invoke 하기
+            MusicInfo musicInfo = MusicTable.Instance.GetMusicInfo(playable.index);
+            onLoadMusicInfo.Invoke(musicInfo);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
