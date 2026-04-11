7819a19b49eeeaca63745e00bd9ad1b7264af0ab
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-04-12 04:32:00 +0900

    난이도 표시 별를 관리하는 매니저의 각 별의 phase 설정 함수 구현

--- Assets/Scripts/MainMenu/MusicSelect/DifficuityStar/DifficuityStarManager.cs
+++ Assets/Scripts/MainMenu/MusicSelect/DifficuityStar/DifficuityStarManager.cs
@@ -6,6 +6,17 @@ public class DifficuityStarManager : MonoBehaviour
     [SerializeField]
     private List<DifficuityStar> difficuityStars;
 
+    private void OnChangeDifficuity(int difficuity)
+    {
+        for (int i = 0; i < difficuityStars.Count; i++)
+        {
+            DifficuityStar difficuityStar = difficuityStars[i];
+            int phase = (difficuity + difficuityStars.Count - i + 1) / difficuityStars.Count;
+
+            difficuityStar.SetPhase(phase);
+        }
+    }
+
     //(n + m - p) / m
     // p = 별들의 각 위치
     // n = 현재 난이도
