e3dff19a819226c630fc3d3347cc02a57d0adfca
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-04-12 07:24:05 +0900

    음악 선택 매니저 구현 마우스와 키의 입력을 받아 value를 조정하고 invoke 하는 클래스

--- Assets/Scripts/MainMenu/MusicSelect/MusicSelectManager.cs
+++ Assets/Scripts/MainMenu/MusicSelect/MusicSelectManager.cs
@@ -1,18 +1,124 @@
+using System.Collections;
 using SimpleActions;
+using Tables.MusicTable;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Utils;
 
-public class MusicSelectManager : MonoBehaviour
+public class MusicSelectManager : Managers<MusicSelectManager>
 {
-    SimpleEvent<float> onChangePosition = new SimpleEvent<float>();
+    public SimpleEvent<float> onChangePosition = new SimpleEvent<float>();
+
+    public SimpleEvent<int> onChangeIndex = new SimpleEvent<int>();
+
+    [SerializeField]
+    private float position;
+    [SerializeField]
+    private int index;
+
+    private int maxIndex;
+
+    private bool isDown = false;
+
+    private Coroutine mouseInputCoroutine;
 
     private void Awake()
     {
-
-
+        Singleton(false);
     }
-    void Update()
+
+    private void Start()
     {
-        // if (Input.GetAxis())
+        if (MenuAssetLoadManager.Instance.assetLoadRecoder.IsAllComplete())
+        {
+            GetMax();
+        }
+        else
+        {
+            MenuAssetLoadManager.Instance.AssetLoaderBind(GetMax);
+        }
+    }
+
+    private void GetMax()
+    {
+        maxIndex = MusicTable.Instance.GetPlayableMusic().Length - 1;
+    }
+    private void Update()
+    {
+        KeyDetection();
+        MouseDetection();
+    }
+
+    private void KeyDetection()
+    {
+        bool upInput = Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Horizontal") == 1;
+        bool downInput = Input.GetAxisRaw("Vertical") == -1 || Input.GetAxisRaw("Horizontal") == -1;
+
+        // 꾹 눌러도 한번만 작동하게
+        if (!upInput && !downInput) isDown = false;
+        if (isDown) return;
+
+        // 위
+        if (upInput)
+        {
+            isDown = true;
+            ChangePosition(1);
+        }
+
+        // 아래
+        if (downInput)
+        {
+            isDown = true;
+            ChangePosition(-1);
+        }
+    }
+
+    private void MouseDetection()
+    {
+        if (mouseInputCoroutine != null) return;
+
+        bool mouseInput = Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1);
+        if (mouseInput)
+        {
+            mouseInputCoroutine = StartCoroutine(MouseDetectionCoroutine());
+        }
+    }
+
+    private IEnumerator MouseDetectionCoroutine()
+    {
+        bool mouseInput = true;
+
+        float startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+        while (mouseInput)
+        {
+            float currentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+            ChangePosition(-(startPos - currentPos));
+
+            startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+            mouseInput = Input.GetMouseButton(0) || Input.GetMouseButton(1);
+            yield return null;
+        }
+
+        ChangePosition(-position);
+        this.SafeStopCoroutine(ref mouseInputCoroutine);
+    }
+
+    private void ChangePosition(float value)
+    {
+        float sum = value + index + position;
+        if (sum <= -0.4f) value = Mathf.Max(sum, -0.4f) - (index + position);
+        if (sum >= maxIndex + 0.4f) value = Mathf.Min(sum, maxIndex + 0.4f) - (index + position);
+
+        position += value;
+        onChangePosition.Invoke(position);
+
+        int roundedPosition = Mathf.RoundToInt(position);
+        if (roundedPosition != 0)
+        {
+            index += roundedPosition;
+            position -= roundedPosition;
+
+            onChangeIndex.Invoke(index);
+        }
     }
 }
