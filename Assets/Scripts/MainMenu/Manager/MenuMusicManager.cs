edc056c85851650fce6c48977f79da1e09abf97e
Tivm <109393040+liiililiil@users.noreply.github.com>
2026-05-20 01:54:46 +0900

    노래를 로딩할때 backgroundinfo를 따로 로딩하게 변경 및 소스를 가져오기 말고 생성하는 방식으로 변경

--- Assets/Scripts/MainMenu/Manager/MenuMusicManager.cs
+++ Assets/Scripts/MainMenu/Manager/MenuMusicManager.cs
@@ -10,9 +10,7 @@ using Types.Utils;
 
 public class MenuMusicManager : Managers<MenuMusicManager>
 {
-    [SerializeField]
     private AudioSource audioSourceOne;
-    [SerializeField]
     private AudioSource audioSourceTwo;
 
 
@@ -21,12 +19,15 @@ public class MenuMusicManager : Managers<MenuMusicManager>
     private MMLoop loop;
     private MMSourceManager sourceManager;
     private MMStateChanger stateChanger;
+
+
     public SimpleEvent onBeat { get => conductor.OnBeat; }
     public float beatPerSec { get => conductor.beatPerSec; }
     public int beat { get => conductor.beat; }
     public SimpleEvent onUpdate { get; private set; } = new SimpleEvent();
     public SimpleEvent<float> onTimeChange { get; private set; } = new SimpleEvent<float>();
     public SimpleEvent<MusicIndex> onClipChange { get; private set; } = new SimpleEvent<MusicIndex>();
+    public SimpleEvent<MusicIndex> onMusicInfoChange { get; private set; } = new SimpleEvent<MusicIndex>();
     public SimpleEvent<AudioSource, AudioSource> onSourceChange { get; private set; } = new SimpleEvent<AudioSource, AudioSource>();
 
     //로딩할 노래
@@ -34,6 +35,9 @@ public class MenuMusicManager : Managers<MenuMusicManager>
     private void Awake()
     {
         Singleton(false);
+
+        audioSourceOne = gameObject.AddComponent<AudioSource>();
+        audioSourceTwo = gameObject.AddComponent<AudioSource>();
 
         conductor = GetComponent<MMConductor>();
         conductor.Bind(this);
@@ -58,7 +62,7 @@ public class MenuMusicManager : Managers<MenuMusicManager>
     {
         MusicLoad(TARGET_MUSIC);
     }
-    private void MusicLoad(MusicIndex musicIndex)
+    private void MusicLoad(MusicIndex musicIndex, bool isloadbackgroundInfo = true)
     {
         // 클립 등록
         AudioClip audioClip = MusicTable.Instance.GetMusic(musicIndex).audioClip;
@@ -68,15 +72,24 @@ public class MenuMusicManager : Managers<MenuMusicManager>
         audioSourceTwo.volume = 0;
         sourceManager.Play();
 
+        if (isloadbackgroundInfo)
+        {
+            onMusicInfoChange.Invoke(musicIndex);
+        }
+
         onClipChange.Invoke(musicIndex);
         onTimeChange.Invoke(0);
+
+
     }
     private void Update()
     {
         onUpdate.Invoke();
     }
+
+    public void ClipChange(MusicIndex musicIndex, bool isloadbackgroundInfo = true) => MusicLoad(musicIndex, isloadbackgroundInfo);
     public void SourceChanged() => sourceManager.AudioSourceChange();
-    public void RangeLoop(FloatRange range, bool isGoingToPart = true) => loop.RangeLoop(range, isGoingToPart);
+    public void RangeLoop(FloatRange range, bool isSmoothLoop = false, bool adaptiveloop = true, bool isGoingToPart = true) => loop.RangeLoop(range, isSmoothLoop, adaptiveloop, isGoingToPart);
     public void RangeStart(FloatRange range, FloatRange nextLoop = null, bool isGoingToPart = true) => loop.RangeStart(range, nextLoop, isGoingToPart);
     public void Stop() => sourceManager.Stop();
     public void SetVolume(float value) => sourceManager.SetVolume(value);
